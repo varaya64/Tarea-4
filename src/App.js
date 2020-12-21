42f42ff4d207271e132c15f7c70fdd95dd158746
varaya64 <v.valentina@estudiantec.cr>
2020-12-20 18:12:26 -0600

    Works

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
+import React from "react"
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Table, Button, Container, Modal, ModalBody, ModalHeader, FormGroup, ModalFooter} from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const data = [
+  {id: 1, nombre: "Carmen", edad: "20"},
+];
+
+class App extends React.Component{
+state={
+  data: data
+}
+
+render(){
+  return(
+    <>
+    <Container>
+    <br />
+    <Button color = "success">Insertar Nuevo Empleado</Button>
+    <br /><br />
+
+    <Table>
+      <thead><tr>
+      <th>Id</th>
+      <th>Name</th>
+      <th>Age</th>
+      <th>Actions</th></tr></thead>
+      <tbody>
+        {this.state.data.map((elemento)=>(
+          <tr>
+            <td>{elemento.id}</td>
+            <td>{elemento.nombre}</td>
+            <td>{elemento.edad}</td> 
+            <td>{elemento.id}</td> 
+            <td><Button color="primary">Editar</Button></td>{" "}
+            <td><Button color="danger">Eliminar</Button></td>
+          </tr>
+        ))}
+
+      </tbody>
+    </Table>
+
+    </Container>
+    </>
+
+  )
+}
+
 }
 
 export default App;
